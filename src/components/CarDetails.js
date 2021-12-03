95c177a67cd21a2925f7e5958ef3eb7cf52cf438
Anasbnth <anas.bentaher@student.ehb.be>
2021-12-03 15:09:06 +0100

    Finish showdetails

--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -40,7 +40,7 @@ function CarDetails(props) {
 
               <div className="carImage">
               <h2>Mercedes Classe A 180 160pk</h2>
-              <img src="assets/images/line-dec.png" alt=""/>
+              <img className="img-fluid" src="logo192.png" alt=""/>
               </div>
 
         <div className="boxGroup">
@@ -145,6 +145,9 @@ function CarDetails(props) {
                 <label>Description:</label>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
               </div>
+
+
+              <img className="img-fluid" src="logo192.png" alt=""/>
 
             
             
